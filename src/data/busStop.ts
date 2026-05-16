e29f12e22c077087ceec85516fda33f86712b0f0
Sosus1009 <145317857+Sosus1009@users.noreply.github.com>
2026-05-16 19:17:06 +0900

    feat: 버스 정류장 데이터 추가

---
+++ src/data/busStop.ts
@@ -0,0 +1,22 @@
+import { BusStop } from "../types/busStop";
+
+export const busStops: BusStop[] = [
+  {
+    id: 1,
+    name: "옥동사거리 정류장",
+    latitude: 36.5660,
+    longitude: 128.7265
+  },
+  {
+    id: 2,
+    name: "옥동주민센터앞 정류장",
+    latitude: 36.5652,
+    longitude: 128.7248
+  },
+  {
+    id: 3,
+    name: "옥동중앙공원 정류장",
+    latitude: 36.5642,
+    longitude: 128.7235
+  }
+];
