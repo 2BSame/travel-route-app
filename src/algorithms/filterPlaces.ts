7b89d6f5cf748ed76f3d902b2eda0e355fb68268
SORA <dlfdydlf30@naver.com>
2026-05-18 18:03:38 +0900

    가이드북에 따르면 해당 파일들은 필요하지 않거나, 이름이 다름, 일괄적으로 삭제.

--- src/algorithms/filterPlaces.ts
+++
@@ -1,5 +0,0 @@
-import { Place } from "../types/place";
-
-export function filterPlacesByMood(places: Place[], selectedMood: string) {
-  return places.filter((place) => place.moodTags.includes(selectedMood));
-}
