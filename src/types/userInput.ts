5a15731c6c3ca2194d98ba2ca4b9c95f93befffd
SORA <dlfdydlf30@naver.com>
2026-05-18 19:53:47 +0900

    시작 정류장과 도착 정류장 업데이트

--- src/types/userInput.ts
+++ src/types/userInput.ts
@@ -1,7 +1,10 @@
 export interface UserRouteInput {
   startTime: string; // 예: "09:00"
-  totalAvailableTime: number; // 분 단위로 입력.
-  themes: number; // 비트마스크 합 (예: themeMask.MOOD | themeMask.QUIET), input을 구현할 때, place안의 마스크를 불러올 것.
-  categories: number; // 비트마스크 합
+  totalAvailableTime: number; // 분 단위
+  themes: number; // 비트마스크
+  categories: number; // 비트마스크
+  mealRequired: boolean;
   maxPlaceCount: number;
+  startBusStopId: number; // 출발할 정류장 ID
+  endBusStopId: number;   // 최종적으로 돌아올 정류장 ID
 }
